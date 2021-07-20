19e6761a70e80d9e86cea26e3862a4b1b1b45dff
Michael Shapiro <sifka@earthlink.net>
2021-07-20 12:24:24 +0100

    Looked at the overlap between gene clusters. Modified Hallmark heatmap to take log + 1 of -log p-value

--- ASmallCradle/hallmarkHeatmap.R
+++ ASmallCradle/hallmarkHeatmap.R
@@ -15,27 +15,29 @@ geneSets = readLines('~/geneSets/mouse/h.all.v7.2.symbols.gmt')
 geneSets = str_split(geneSets,'\t')
 which = c('log','density')
 
-res = 1
-for(assay in assays)
-{
-    for(whichFunction in which)
+for(res in 1:2)
+    for(assay in assays)
     {
-        clusterDF = Read.Table(paste0(assay,'_resolution_',res,'/geneClusters.txt'))
-        background = 21281
-        M = getGeneSetsVsClustersMatrix(geneSets,
-                                        clusterDF,
-                                        whichFunction,
-                                        background)
-
-        figName = paste0(assay,'_resolution_',res,'/hallmarkHeatMap_',
-                         whichFunction,'.jpg')
-        jpeg(figName,
-             height=12,width=12,units='in',res=200)
-        pheatmap(M,
-                 treeheight_row=0,
-                 treeheight_col=0)
-        dev.off()
+        for(whichFunction in which)
+        {
+            clusterDF = Read.Table(paste0(assay,'_resolution_',res,'/geneClusters.txt'))
+            background = 21281
+            M = getGeneSetsVsClustersMatrix(geneSets,
+                                            clusterDF,
+                                            whichFunction,
+                                            background)
+            if(whichFunction == 'log')
+                M = log10(M+1)
+            
+            figName = paste0(assay,'_resolution_',res,'/hallmarkHeatMap_',
+                             whichFunction,'.jpg')
+            jpeg(figName,
+                 height=12,width=12,units='in',res=200)
+            pheatmap(M,
+                     treeheight_row=0,
+                     treeheight_col=0)
+            dev.off()
+        }
     }
 
-}
     
