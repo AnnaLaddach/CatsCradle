0b7e51aa7b81d86e1b4e2cc726001b029c774f1c
Michael Shapiro <michael.shapiro@crick.ac.uk>
2023-08-25 14:22:05 +0100

    Commenting and cleaning up code. Adding data and examples. Added functions for complement distance.

--- R/CatsCradle.R
+++ R/CatsCradle.R
@@ -17,6 +17,8 @@
 #' @return A Seurat object
 #' @export
 #' @import Seurat
+#' @examples
+#' STranspose = transposeSeuratObject(S)
 transposeSeuratObject = function(f,active.assay=f@active.assay,
                                  npcs=30,dims=1:20,res=1)
 {
@@ -40,35 +42,6 @@ transposeSeuratObject = function(f,active.assay=f@active.assay,
 }
 
 ## ###################################################
-## This retrieves the expression matrix:
-##
-## f - a Seurat object
-getExpression = function(f)
-{
-  M = f@assays[[f@active.assay]]@data
-  M = as.matrix(M)
-  rownames(M) = rownames(f)
-  
-  return(M)
-}
-
-## ###################################################
-## This computes z-scores:
-##
-## M - a matrix
-zScores = function(M)
-{
-  mu = rowMeans(M)
-  M = M - mu
-  
-  for(i in 1:nrow(M))
-    M[i,] = M[i,] / sd(M[i,])
-  
-  return(M)
-}
-
-
-## ###################################################
 #' This computes average expression of each gene cluster in
 #' each cell cluster and returns the result as a matrix
 #'
@@ -76,6 +49,8 @@ zScores = function(M)
 #' @param fPrime - The Seurat object of genes
 #' @return A matrix of the average expression
 #' @export
+#' @examples
+#' M = getAverageExpression(S,STranspose)
 getAverageExpressionMatrix = function(f,fPrime)
 {
   f$seurat_clusters = as.character(f$seurat_clusters)
@@ -86,10 +61,9 @@ getAverageExpressionMatrix = function(f,fPrime)
   geneCluster = unique(fPrime$seurat_clusters)
   geneCluster = geneCluster[order(as.numeric(geneCluster))]
   
-  ## Get z-scored expression:
-  X = getExpression(f)
-  X = zScores(X)
-  
+  ## Get assay data:
+  X = GetAssayData(f,slot='scale')
+   
   M = matrix(0,nrow=length(cellCluster),ncol=length(geneCluster))
   rownames(M) = cellCluster
   colnames(M) = geneCluster
@@ -114,10 +88,12 @@ getAverageExpressionMatrix = function(f,fPrime)
 #' @param ccTag - a prefix for the row (cell cluster) names
 #' @param gcTag - a prefix for the column (gene cluster) names
 #' @export
+#' @examples
+#' averageExpMatrix = tagRowAndColNames(averageExpMatrix)
 tagRowAndColNames = function(M,ccTag='CC_',gcTag='GC_')
 {
-  rownames(M) = paste0(rownames(M),ccTag)
-  colnames(M) = paste0(colnames(M),gcTag)
+  rownames(M) = paste0(ccTag,rownames(M))
+  colnames(M) = paste0(gcTag,colnames(M))
   
   return(M)
 }
@@ -129,28 +105,16 @@ tagRowAndColNames = function(M,ccTag='CC_',gcTag='GC_')
 #' @param M - An average gene expression matrix.
 #' @return A data frame with columns cellCluster, geneCluster
 #' and average expression
+#' @import reshape2
 #' @export
+#' @examples
+#' averageExpDF = getAverageExpressionDF(averageExpMatrix)
 getAverageExpressionDF = function(M)
 {
-  N = nrow(M) * ncol(M)
-  df = data.frame(cellCluster=character(N),
-                  geneCluster=character(N),
-                  expression=numeric(N))
-  
-  finger = 1
-  for(i in 1:nrow(M))
-  {
-    for(j in 1:ncol(M))
-    {
-      df$cellCluster[finger] = rownames(M)[i]
-      df$geneCluster[finger] = colnames(M)[j]
-      df$expression[finger] = M[i,j]
-      
-      finger = finger + 1
-    }
-  }
-  
-  return(df)
+    df = melt(M)
+    names(df) = c('cellCluster','geneCluster','averageExpression')
+
+    return(df)
 }
 
 ## ####################################################
@@ -318,6 +282,12 @@ orderGeneSetPValues = function(M,ascending=TRUE,cutoff=NULL,nameTag='')
 #' @export
 #' @import networkD3
 #' @import stringr
+#' @examples
+#' set.seed(100)
+#' M = matrix(runif(12)-.3,nrow=3)
+#' rownames(M) = as.character(1:3)
+#' colnames(M) = as.character(1:4)
+#' S = sankeyFromMatrix(M)
 sankeyFromMatrix = function(M,disambiguation=c('R_','C_'),
                             fontSize=20,minus='red',plus='blue',
                             height=1200,width=900)
@@ -369,7 +339,7 @@ sankeyFromMatrix = function(M,disambiguation=c('R_','C_'),
   
   p = sankeyNetwork(Links = links, Nodes = nodes, Source = "IDsource", Target = "IDtarget", 
                     Value = "value", NodeID = "name", 
-                    ##colourScale=linkColor,
+                    colourScale=linkColor,
                     LinkGroup="group",
                     fontSize=fontSize,
                     height=height,
@@ -392,6 +362,8 @@ sankeyFromMatrix = function(M,disambiguation=c('R_','C_'),
 #' correspond to geneClusters and the entries give average expression
 #' for each cluster in each cell
 #' @export
+#' @examples
+#' clusterExpression = getGeneClusterAveragesPerCell(S,STranspose)
 getGeneClusterAveragesPerCell = function(f,
                                          fPrime,
                                          cells=colnames(f),
@@ -418,8 +390,8 @@ getGeneClusterAveragesPerCell = function(f,
 ## ####################################################
 #' This gets the clusters in their cannonical order
 #'
-#' Note: seurat_clusters might be either a factor or a numeric.
-#' This will keep the class.
+#' This deals with skullduggery in which seurat_clusters
+#' has been converted to a character or a numeric. 
 #'
 #' @param f - a Seurat object with meta.data column seurat_clusters
 #' @return A vector of these unique values in order
@@ -427,13 +399,14 @@ getGeneClusterAveragesPerCell = function(f,
 getClusterOrder = function(f)
 {
     a = unique(f$seurat_clusters)
-    a = a[order(a)]
+    a = a[order(as.numeric(a))]
 
     return(a)
 }
 
 ## ####################################################
 ## Default graph
+## Used internally
 defaultGraph = function(f)
 {
     graph = paste0(f@active.assay,'_snn')
@@ -452,10 +425,11 @@ defaultGraph = function(f)
 #' nodeB - node names for node B
 #' weight - edge weight
 #' @export
+#' @examples
+#' NN = getNearestNeighborListsSeurat(STranspose)
 getNearestNeighborListsSeurat = function(f, graph=defaultGraph(f)){
   
-    ## convert to dgTMatrix and extract relevant information
-    ## graph = as(f@graphs[[graph]], "dgTMatrix")
+    ## convert to TsparseMatrix and extract relevant information
     graph = as(f@graphs[[graph]], "TsparseMatrix") 
     neighborListDf = data.frame("nodeA" = graph@Dimnames[[1]][graph@i+1],
                                 "nodeB" =  graph@Dimnames[[2]][graph@j+1], 
@@ -477,6 +451,9 @@ getNearestNeighborListsSeurat = function(f, graph=defaultGraph(f)){
 #' neighbor graph as found by getNearestNeighborListsSeurat
 #' @return the neighboring genes
 #' @export
+#' @examples
+#' neighbors = getGeneNeighbors("Ccl6",STranspose)
+#' neighborsAgain = getGeneNeighbors("Ccl6",NN)
 getGeneNeighbors = function(gene,NN)
 {
     if(class(NN) == 'Seurat')
@@ -495,6 +472,8 @@ getGeneNeighbors = function(gene,NN)
 #' @param useWeights - whether to preserve edgeweights.
 #' @return - a matrix with randomised indices for node B
 #' @export
+#' @examples
+#' randomIndices = randomiseNodeIndices(NN,10,TRUE)
 randomiseNodeIndices = function(neighborListDf, n = 100, useWeights = F){
   
   #determine number of edges and create empty matrix for randomised indices
@@ -541,6 +520,8 @@ randomiseNodeIndices = function(neighborListDf, n = 100, useWeights = F){
 #'
 #' @return TRUE or FALSE
 #' @export
+#' @examples
+#' symmetryTest = symmetryCheckNN(NN)
 symmetryCheckNN = function(NN)
 {
     tagA = paste(NN$nodeA,NN$nodeB)
@@ -565,6 +546,8 @@ symmetryCheckNN = function(NN)
 #' @return a nearest neighbors graph
 #' 
 #' @export
+#' @examples
+#' NNStar = symmetrizeNN(NN)
 symmetrizeNN = function(NN)
 {
     ## Is it already symmetric?
@@ -598,9 +581,11 @@ symmetrizeNN = function(NN)
 #' gene name, the radius from the origin at which it is found
 #'
 #' @export
+#' @examples
+#' spheres = combinatorialSpheres(NN,'Ccl6',3)
 combinatorialSpheres = function(NN,origin,radius)
 {
-    stopifnot(symmetryCheckNN(NN))
+    NN = symmetrizeNN(NN)
     
     nodes = unique(NN$nodeA)
     ball = data.frame(nodes,
@@ -636,8 +621,13 @@ combinatorialSpheres = function(NN,origin,radius)
 
         ball$radius[idx] = r
 
-        numFound = sum(ball$radius > -.5)
-        writeLines(paste('   ',numFound))
+        newFound = sum(ball$radius > -.5)
+        writeLines(paste('   ',newFound))
+
+        ## Can only happen in a disconnected graph
+        ## or with radius too large:
+        if(newFound == numFound)
+            break
     }
 
     ## Trim:
@@ -655,7 +645,7 @@ combinatorialSpheres = function(NN,origin,radius)
 ## ####################################################
 #' This function reads in gene sets in .gmt format
 #'
-#' @param gmtFile - a .gmt file containing gene sets 
+#' @param gmtFile - a .gmt file containing gene sets, e.g., Hallmark of GO
 #' @param addDescr - include gene set description (2nd column in .gmt file) in 
 #' gene set name  
 #' @return - A named list of gene sets
@@ -679,10 +669,18 @@ readGmt = function(gmtFile, addDescr = F){
 ## ####################################################
 #' This function annotates genes with terms
 #'
+#' This essentially inverts a list of gene sets.  It takes
+#' a list (e.g., Hallmark or GO) where each list item is a
+#' name of a gene set and gives the genes in that set and
+#' returns a list where each item is a gene and gives the
+#' gene sets that gene is in.
+#'
 #' @param geneSets - a list of gene sets, e.g., as produced by
 #' readGmt
 #' @return - A list where names are genes and values are lists of terms
 #' @export
+#' @examples
+#' annotatedGenes = annotateGenes(hallmark)
 annotateGenes = function(geneSets){
   #this is a bit slow for large collections of gene sets (a few minutes)
   #think about whether it's necessary to speed up
@@ -710,6 +708,9 @@ annotateGenes = function(geneSets){
 #' @param normalise - whether to normalise by set size
 #' @return a numeric
 #' @export
+#' @examples
+#' Myc = annotateGeneAsVector('Myc',hallmark)
+#' MycNormalised = annotateGeneAsVector('Myc',hallmark,TRUE)
 annotateGeneAsVector = function(gene,geneSets,normalise=FALSE)
 {
     N = length(geneSets)
@@ -729,8 +730,8 @@ annotateGeneAsVector = function(gene,geneSets,normalise=FALSE)
 
 
 ## ####################################################
-#' This function predicts the functions of a gene based on the functions of its
-#' neighbours.
+#' This function predicts the functions of a gene based
+#' on the functions of its neighbours.#' 
 #'
 #' @param gene - gene to annotate
 #' @param fPrime - a transposed Seurat object (generated with 
@@ -744,6 +745,9 @@ annotateGeneAsVector = function(gene,geneSets,normalise=FALSE)
 #' Scores are calculated according to the weights of the SNN graph.
 #'
 #' @export
+#' @examples
+#' genesAnno = annotateGenes(hallmark)
+#' predictions = predictTerms('Myc',STranspose,genesAnno)
 predictTerms = function(gene, fPrime, genesAnno, normalise = T,
                         graph=defaultGraph(fPrime)){
   
@@ -793,32 +797,191 @@ predictTerms = function(gene, fPrime, genesAnno, normalise = T,
 
 
 ## ####################################################
-#' This function predicts the functions of all genes based on the functions of 
-#' their neighbours.
+#' This function predicts the functions of all genes based
+#' on the functions of their neighbours.
 #'
 #' @param fPrime - a transposed Seurat object (generated with 
 #' transposeSeuratObject())
 #' @param genesAnno - genes annotated with gene sets
+#' @param genes - the genes to be predicted, defaults to all
+#' @param normalise - whether to normalise to gene set size, defauls
+#' to TRUE
 #' @return - A list where names are genes and values are lists of terms.
 #' The values of the lists of terms are calculated according to the weights
 #' of the edges connecting the neighbors.
 #' @export
-predictTermsAllGenes = function(fPrime,genesAnno, normalise = T,
-                                graph=defaultGraph(fPrime)){
+#' @examples
+#' set.seed(100)
+#' genes = sample(colnames(STranspose),50)
+#' genesAnno = annotateGenes(hallmark)
+#' predictions = predictTermsAllGenes(STranspose,genesAnno,genes)
+predictTermsAllGenes = function(fPrime,
+                                genesAnno,
+                                genes=colnames(fPrime),
+                                normalise = T){
   
-  #determine genes
-  genes = rownames(fPrime@graphs$RNA_snn)
   genesPredictedTerms = list()
   i = 1
   
   #iterate through genes
   for (gene in genes){
     if (i %% 100 == 0){
-      print(paste(i, "genes processed"))
+        writeLines(paste(i, "genes processed"))
     }
     i = i + 1
     genesPredictedTerms[[gene]] = predictTerms(gene, fPrime, genesAnno, 
                                                normalise)
   }
   return(genesPredictedTerms)
+}
+
+## ####################################################
+#' This function computes a p-value for the geometric
+#' clustering of a gene set (in UMAP or PCA reduction)
+#' based on the median distance from its complement to
+#' the set.
+#'
+#' @param fPrime - a transposed Seurat object, i.e. a
+#' Seurat object of genes
+#' @param geneSubset - a subset of the genes which can
+#' be given as a character vector as a logical vector
+#' @param numTrials - the number of random trials to be
+#' carried out for randomised testing. Defaults to 1000.
+#' @param reduction - can be 'UMAP' or 'PCA', defaults
+#' to 'UMAP'
+#' @param numPCs - number of PCs to use if reduction is
+#' 'PCA'
+#' @return A p-value reporting how often a random subset
+#' of the same size is sufficiently clustered to produce
+#' an equally large distance from its complement.
+#' @import pracma
+#' @export
+#' @examples
+#' geneSubset = intersect(colnames(STranspose),hallmark[[1]])
+#' p = getSubsetClusteringPValue(STranspose,geneSubset,100)
+getSubsetClusteringPValue = function(fPrime,
+                                     geneSubset,
+                                     numTrials=1000,
+                                     reduction='UMAP',
+                                     numPCs=10)
+{
+    stats = getSubsetClusteringStatistics(fPrime,
+                                          geneSubset,
+                                          numTrials,
+                                          reduction,
+                                          numPCs)
+    
+    return(stats$pValue)
+}
+
+
+## ####################################################
+#' This function computes statistics for the geometric
+#' clustering of a gene set (in UMAP or PCA reduction)
+#' based on the median distance from its complement to
+#' the set.
+#'
+#' @param fPrime - a transposed Seurat object, i.e. a
+#' Seurat object of genes
+#' @param geneSubset - a subset of the genes which can
+#' be given as a character vector as a logical vector
+#' @param numTrials - the number of random trials to be
+#' carried out for randomised testing. Defaults to 1000.
+#' @param reduction - can be 'UMAP' or 'PCA', defaults
+#' to 'UMAP'
+#' @param numPCs - number of PCs to use if reduction is
+#' 'PCA'
+#' @return A list of statistics resulting from the
+#' testing of randomised subsets of the same size as the
+#' given gene subset.  These include subsetDistance, the 
+#' actual median complement distance; randomSubsetDistance,
+#' the median complement distances for randomised subsets;
+#' pValue, computed by comparing the real and randomised
+#' distances; and zScore, the z-distance of the actual
+#' median distance from the mean of the randomised distances.
+#' @export
+#' @examples
+#' geneSubset = intersect(colnames(STranspose),hallmark[[1]])
+#' stats = getSubsetClusteringStatistics(STranspose,geneSubset,100)
+getSubsetClusteringStatistics = function(fPrime,
+                                         geneSubset,
+                                         numTrials=1000,
+                                         reduction='UMAP',
+                                         numPCs=10)
+{
+    if(reduction == 'UMAP')
+        S = FetchData(fPrime,c('UMAP_1','UMAP_2'))
+
+    if(reduction == 'PCA')
+    {
+        pcs = paste0('PC_',1:numPCs)
+        S = FetchData(fPrime,pcs)
+    }
+
+    ## The coordinates of all the genes in the
+    ## chosen reduction:
+    S = as.matrix(S)
+    rownames(S) = colnames(fPrime)
+
+    answer = runClusteringTrials(S,
+                                 geneSubset,
+                                 numTrials)
+
+    return(answer)
+}
+
+## ####################################################
+## Used internally
+runClusteringTrials = function(S,
+                               geneSubset,
+                               numTrials)
+{
+    if(class(geneSubset) == 'character')
+        geneSubset = rownames(S) %in% geneSubset
+    
+    answer = list()
+    answer$subsetDistance = medianComplementDistance(S,geneSubset)
+
+    randomSubsetDistance = c()
+    for(i in 1:numTrials)
+    {
+        randomSubset = sample(rownames(S),sum(geneSubset))
+        randomSubset = rownames(S) %in% randomSubset
+        randomSubsetDistance[i] = medianComplementDistance(S,randomSubset)
+    }
+    answer$randomSubsetDistance = randomSubsetDistance
+
+    count = sum(randomSubsetDistance > answer$subsetDistance)
+    count = max(count,1)
+
+    answer$pValue = count / numTrials
+
+    mu = mean(randomSubsetDistance)
+    std = sd(randomSubsetDistance)
+    answer$zScore = (answer$subsetDistance - mu) / std
+
+    return(answer)
+}
+
+## ####################################################
+## Used internally
+medianComplementDistance = function(S,geneSubset)
+{
+    if(class(geneSubset) == 'character')
+        geneSubset = colnames(S) %in% geneSubset
+    
+    ## The complement:
+    A = S[!geneSubset,]
+    
+    ## The subset:
+    B = S[geneSubset,]
+    
+    D = distmat(A,B)
+    
+    rowMin = c()
+    for(i in 1:nrow(D))
+        rowMin[i] = min(D[i,])
+    d = median(rowMin)
+    
+    return(d)
 }
---
+++ R/data.R
@@ -0,0 +1,98 @@
+
+## ####################################################
+#' S
+#'
+#' A Seurat object of 2000 genes by 1445 cells.
+#'
+#' @format A Seurat object
+#'
+#' \describe{
+#' A Seurat object of cells.  It includes a UMAP of the
+#' cells and annotated clustering into cell types.
+#' }  
+#' @source This is subset from the data associated with
+#'     https://www.nature.com/articles/s41586-021-04006-z
+"S"
+
+## ####################################################
+#' STranspose
+#'
+#' A Seurat object of 1445 cells by 2000 genes
+#'
+#' @format A Seurat object
+#' \describe{
+#' This is the transpose of S. It includes a UMAP of the
+#' genes and and their clustering into gene types.
+#' }
+#' @source Produced from S by transposeSeuratObject()
+"STranspose"
+
+## ####################################################
+#' averageExpMatrix
+#'
+#' The average expression matrix for cell clusters of S
+#' and gene clusters of STranspose.
+#'
+#' @format A 30 x 13 matrix
+#' \describe{
+#' This gives the average gene expression for each of the
+#' gene clusters in each of the cell clusters.
+#' }
+#' @source Produced from S and STranspose by
+#' getAverageExpression()
+"averageExpMatrix"
+
+## ####################################################
+#' hallmark
+#'
+#' The mouse hallmark gene sets.
+#'
+#' @format A named list
+#' \describe{
+#' The mouse hallmark gene sets.
+#' }
+#' @source https://www.gsea-msigdb.org/gsea/msigdb/mouse/collections.jsp
+
+## ####################################################
+#' shorterHallmark
+#'
+#' An abbreviation of mouse hallmark gene sets.
+#'
+#' @format A named list
+#' \describe{
+#' The alphabetically first ten gene hallmark gene sets.
+#' }
+#' @source Abbreviated from hallmark
+"shorterHallmark"
+
+## ####################################################
+#' clusterDF
+#'
+#' A data frame of genes and their gene cluster membership
+#'
+#' @format A data frame with columns gene and geneCluster
+#' \describe{
+#' This gives the cluster membership of the genes of STranspose.
+#' }
+#' @source Extracted from STranspose
+"clusterDF"
+
+## ####################################################
+#' NN
+#'
+#' The nearest neighbor graph of STranspose
+#'
+#' @format A data frame with columns nodeA, nodeB and weight
+#' \describe{
+#' This gives the weigthed edges of the nearest neighbor graph
+#' of the genes in STranspose.
+#' }
+#' @source Extracted from STranspose by getNearestNeighborListsSeurat
+"NN"
+
+
+
+
+
+
+
